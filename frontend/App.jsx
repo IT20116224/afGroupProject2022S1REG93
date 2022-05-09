bd46db14c514a665ac166175da3579afd5714125
nirasha jayasiri <nirashajayasiri@gmail.com>
2022-05-09 11:36:50 +0530

    initializing frontend

---
+++ frontend/App.jsx
@@ -0,0 +1,13 @@
+function App(){
+    return(
+        <div>
+            AF Group project
+
+            use routes to connect components
+            use react router dom version 5.2.0 if you get errors.
+
+        </div>
+    )
+}
+
+export default App;
